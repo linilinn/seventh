8a843369131e67ad41fef3b4734d252d31f75be4
linilinn <125601952+linilinn@users.noreply.github.com>
2023-05-06 06:02:33 +0300

    fix

--- app/src/main/java/com/example/seventh/ui/fragments/AddDrinkFragment.java
+++ app/src/main/java/com/example/seventh/ui/fragments/AddDrinkFragment.java
@@ -9,21 +9,36 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import com.example.seventh.R;
+import com.example.seventh.data.models.DrinkModel;
 import com.example.seventh.databinding.FragmentAddDrinkBinding;
+import com.example.seventh.ui.viewmodels.DrinkViewModel;
 
 public class AddDrinkFragment extends Fragment {
     private FragmentAddDrinkBinding binding;
+    private DrinkViewModel drinkViewModel;
     public AddDrinkFragment() {
         super(R.layout.fragment_add_drink);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        drinkViewModel = new ViewModelProvider(this).get(DrinkViewModel.class);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentAddDrinkBinding.inflate(inflater, container, false);
+        Bundle args = getArguments();
+        if (args != null && args.containsKey("RESULT_OK_DRINK") && args.containsKey("RESULT_OK_DRINKDESCRIPTION")) {
+            DrinkModel drink = new DrinkModel(args.getString("RESULT_OK_DRINK"), args.getString("RESULT_OK_DRINKDESCRIPTION"), R.drawable.drink);
+            drinkViewModel.insert(drink);
+        }
         return binding.getRoot();
     }
 
--- app/src/main/java/com/example/seventh/ui/fragments/AddFoodFragment.java
+++ app/src/main/java/com/example/seventh/ui/fragments/AddFoodFragment.java
@@ -9,21 +9,36 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import com.example.seventh.R;
+import com.example.seventh.data.models.FoodModel;
 import com.example.seventh.databinding.FragmentAddFoodBinding;
+import com.example.seventh.ui.viewmodels.FoodViewModel;
 
 public class AddFoodFragment extends Fragment {
     private FragmentAddFoodBinding binding;
+    private FoodViewModel foodViewModel;
     public AddFoodFragment() {
         super(R.layout.fragment_add_food);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        foodViewModel = new ViewModelProvider(this).get(FoodViewModel.class);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentAddFoodBinding.inflate(inflater, container, false);
+        Bundle args = getArguments();
+        if (args != null && args.containsKey("RESULT_OK_FOOD") && args.containsKey("RESULT_OK_FOODDESCRIPTION")) {
+            FoodModel food = new FoodModel(args.getString("RESULT_OK_FOOD"), args.getString("RESULT_OK_FOODDESCRIPTION"), R.drawable.food);
+            foodViewModel.insert(food);
+        }
         return binding.getRoot();
     }
 
